ababd772eb92e89dceae1c06bb2d6e95c3d1e434
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2021-12-21 20:11:13 +0600

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,99 +1,99 @@
-import { Add, Remove } from "@material-ui/icons";
-import { useSelector } from "react-redux";
-import styled from "styled-components";
-import Announcement from "../components/Announcement";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import { mobile } from "../responsive";
-import StripeCheckout from "react-stripe-checkout";
-import { useEffect, useState } from "react";
-import { userRequest } from "../requestMethods";
-import { useHistory } from "react-router";
+import { Add, Remove } from '@material-ui/icons'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import { mobile } from '../responsive'
+import StripeCheckout from 'react-stripe-checkout'
+import { useEffect, useState } from 'react'
+import { userRequest } from '../requestMethods'
+import { useHistory } from 'react-router'
 
-const KEY = process.env.REACT_APP_STRIPE;
+const KEY = process.env.REACT_APP_STRIPE
 
-const Container = styled.div``;
+const Container = styled.div``
 
 const Wrapper = styled.div`
   padding: 20px;
-  ${mobile({ padding: "10px" })}
-`;
+  ${mobile({ padding: '10px' })}
+`
 
 const Title = styled.h1`
   font-weight: 300;
   text-align: center;
-`;
+`
 
 const Top = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 20px;
-`;
+`
 
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
-  border: ${(props) => props.type === "filled" && "none"};
+  border: ${(props) => props.type === 'filled' && 'none'};
   background-color: ${(props) =>
-    props.type === "filled" ? "black" : "transparent"};
-  color: ${(props) => props.type === "filled" && "white"};
-`;
+    props.type === 'filled' ? 'black' : 'transparent'};
+  color: ${(props) => props.type === 'filled' && 'white'};
+`
 
 const TopTexts = styled.div`
-  ${mobile({ display: "none" })}
-`;
+  ${mobile({ display: 'none' })}
+`
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
   margin: 0px 10px;
-`;
+`
 
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
-  ${mobile({ flexDirection: "column" })}
-`;
+  ${mobile({ flexDirection: 'column' })}
+`
 
 const Info = styled.div`
   flex: 3;
-`;
+`
 
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
-  ${mobile({ flexDirection: "column" })}
-`;
+  ${mobile({ flexDirection: 'column' })}
+`
 
 const ProductDetail = styled.div`
   flex: 2;
   display: flex;
-`;
+`
 
 const Image = styled.img`
   width: 200px;
-`;
+`
 
 const Details = styled.div`
   padding: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-`;
+`
 
-const ProductName = styled.span``;
+const ProductName = styled.span``
 
-const ProductId = styled.span``;
+const ProductId = styled.span``
 
 const ProductColor = styled.div`
   width: 20px;
   height: 20px;
   border-radius: 50%;
   background-color: ${(props) => props.color};
-`;
+`
 
-const ProductSize = styled.span``;
+const ProductSize = styled.span``
 
 const PriceDetail = styled.div`
   flex: 1;
@@ -101,31 +101,31 @@ const PriceDetail = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const ProductAmountContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 20px;
-`;
+`
 
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
-  ${mobile({ margin: "5px 15px" })}
-`;
+  ${mobile({ margin: '5px 15px' })}
+`
 
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
-  ${mobile({ marginBottom: "20px" })}
-`;
+  ${mobile({ marginBottom: '20px' })}
+`
 
 const Hr = styled.hr`
   background-color: #eee;
   border: none;
   height: 1px;
-`;
+`
 
 const Summary = styled.div`
   flex: 1;
@@ -133,23 +133,23 @@ const Summary = styled.div`
   border-radius: 10px;
   padding: 20px;
   height: 50vh;
-`;
+`
 
 const SummaryTitle = styled.h1`
   font-weight: 200;
-`;
+`
 
 const SummaryItem = styled.div`
   margin: 30px 0px;
   display: flex;
   justify-content: space-between;
-  font-weight: ${(props) => props.type === "total" && "500"};
-  font-size: ${(props) => props.type === "total" && "24px"};
-`;
+  font-weight: ${(props) => props.type === 'total' && '500'};
+  font-size: ${(props) => props.type === 'total' && '24px'};
+`
 
-const SummaryItemText = styled.span``;
+const SummaryItemText = styled.span``
 
-const SummaryItemPrice = styled.span``;
+const SummaryItemPrice = styled.span``
 
 const Button = styled.button`
   width: 100%;
@@ -157,31 +157,33 @@ const Button = styled.button`
   background-color: black;
   color: white;
   font-weight: 600;
-`;
+`
 
 const Cart = () => {
-  const cart = useSelector((state) => state.cart);
-  const [stripeToken, setStripeToken] = useState(null);
-  const history = useHistory();
+  const cart = useSelector((state) => state.cart)
+  const [stripeToken, setStripeToken] = useState(null)
+  const history = useHistory()
 
   const onToken = (token) => {
-    setStripeToken(token);
-  };
+    setStripeToken(token)
+  }
 
   useEffect(() => {
     const makeRequest = async () => {
       try {
-        const res = await userRequest.post("/checkout/payment", {
+        const res = await userRequest.post('/checkout/payment', {
           tokenId: stripeToken.id,
           amount: 500,
-        });
-        history.push("/success", {
+        })
+        history.push('/success', {
           stripeData: res.data,
-          products: cart, });
+          products: cart.total,
+        })
       } catch {}
-    };
-    stripeToken && makeRequest();
-  }, [stripeToken, cart.total, history]);
+    }
+    stripeToken && makeRequest()
+  }, [stripeToken, cart, history])
+
   return (
     <Container>
       <Navbar />
@@ -264,7 +266,7 @@ const Cart = () => {
       </Wrapper>
       <Footer />
     </Container>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
